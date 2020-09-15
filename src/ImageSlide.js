95afa7973932a3b2baf1f0c2dedf437178a0f65e
Meera Jose Mampilly <meerajm@gmail.com>
2020-09-15 12:21:40 +0200

    added the onClick functionality for the thumbnail images

--- src/ImageSlide.js
+++ src/ImageSlide.js
@@ -4,20 +4,30 @@ import "./ImageSlide.css";
 const ImageSlide = (props) => {
   const url = props.url;
   const photos = props.photos;
+  const setCurrentPhotoIndex = props.setCurrentPhotoIndex;
   const styles = {
     backgroundImage: `url(${url})`,
   };
 
+  function handlePhotoClick(i) {
+    setCurrentPhotoIndex(i);
+  }
+
+  var allPhotos = photos.map((photo, i) => {
+    return (
+      <img
+        key={i}
+        className="thumbnail-img"
+        onClick={() => handlePhotoClick(i)}
+        src={photo}
+        alt="No photo available."
+      />
+    );
+  });
   return (
     <div>
       <div className="image-slide" style={styles}></div>
-      <section className="thumbnails">
-        <img className="thumbnail-img" src={photos[0]}></img>
-        <img className="thumbnail-img" src={photos[1]}></img>
-        <img className="thumbnail-img" src={photos[2]}></img>
-        <img className="thumbnail-img" src={photos[3]}></img>
-        <img className="thumbnail-img"src={photos[4]}></img>
-      </section>
+      <section className="thumbnails">{allPhotos}</section>
     </div>
   );
 };
